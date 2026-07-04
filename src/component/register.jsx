1e0667bd14c911184f933c8a18c9a7b4748be811
Pinki Mandal <99741856+pinki-mandal@users.noreply.github.com>
2026-07-04 18:03:55 +0530

    Update register.jsx

--- src/component/register.jsx
+++ src/component/register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API_BASE_URL = "http://localhost:5000"
+const API_BASE_URL = "http://localhost:5000/api"
 
 function Register() {
   const [mode, setMode] = useState('login')
