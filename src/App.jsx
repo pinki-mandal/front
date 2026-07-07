3c3d6e85b4d10de245b8a5b1407ed9687b0f0eec
Pinki Mandal <99741856+pinki-mandal@users.noreply.github.com>
2026-07-07 16:36:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import Register from './component/register'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./component/register";
+import Dashboard from "./component/dashboard";
 
 function App() {
-  return <Register />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
